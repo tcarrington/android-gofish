7d0199b51af9e4e0fc5e334308c30c1806ecec37
Taylor Carrington <taylor.carrington91@gmail.com>
2013-11-18 22:16:17 -0800

    began work on deck of cards

---
+++ GoFish/src/main/java/go/fish/Card.java
@@ -0,0 +1,14 @@
+package go.fish;
+
+/**
+ * Created by Taylor on 11/18/13.
+ */
+public class Card {
+    private String[] suit = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    private String[] rank = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King"};
+
+    public Card(String suit, String rank) {
+        this.suit[] = suit;
+        this.rank[] = rank;
+    }
+}
---
+++ GoFish/src/main/java/go/fish/DeckOfCards.java
@@ -0,0 +1,11 @@
+package go.fish;
+
+/**
+ * Created by Taylor on 11/18/13.
+ */
+public class DeckOfCards {
+
+    public Card buildDeck() {
+
+    }
+}
